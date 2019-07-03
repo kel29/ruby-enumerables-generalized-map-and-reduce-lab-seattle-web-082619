e17aeeb7682de49304eb9c0af503298b526816cb
Krista <krista.loven.87@gmail.com>
2019-07-03 13:19:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
 # Your Code Here
+def map(arr)
+  arr.map { |i| yield i }
+end
+
+def reduce(arr, v = nil)
+  if v
+    s = v
+    i = 0
+  else
+    s = arr[0]
+    i = 1
+  end
+  while i < arr.length
+    s = yield(s, arr[i])
+    i += 1
+  end
+end
